e68be81a6a6ee14dd603246b1f9dfd3b6bc9c348
obicho <cho@Tsais-MacBook-Pro.local>
2014-02-16 13:17:45 -0800

    fixed BOM issued

--- TextFormatter.py
+++ TextFormatter.py
@@ -3,6 +3,7 @@
 # Reads line by line and print them a pageful at a time
 
 import textwrap, re
+import codecs
 import Image, ImageFont, ImageDraw
 
 # Setup
@@ -12,13 +13,21 @@ w, h = (264, 176)
 HEIGHT = 11
 wrapper = textwrap.TextWrapper(replace_whitespace = False, width = 40)
 
+# Remove unwanted, non-printable characters including BOM
+def cleanse(line):
+	newline = line
+	if (line.startswith(codecs.BOM_UTF8)):
+		print 'need cleansing'
+		newline = line[3:]
+	
+	return newline;
 
 # Draws a screen full (page) of text
 def drawPage(text):
 	bg = Image.new('RGBA', (w, h), "#FFFFFF")
 	draw = ImageDraw.Draw(bg)
 	y_text = 10
-	
+
 	#feed textwrap one paragraph at a time. accomodate for carriage return, newline various os
 	for paragraph in re.split('\r\n\r\n|\n\n', text): 
 		print '-------------'
@@ -26,12 +35,17 @@ def drawPage(text):
 
 		lines = wrapper.wrap(paragraph)
 		for line in lines:
-		    draw.text((LEFT_MARGIN, y_text), line, font = font, fill = 'black')
-		    y_text += HEIGHT
+			simpleLine = cleanse(line)
+			draw.text((LEFT_MARGIN, y_text), simpleLine, font = font, fill = 'black')
+			y_text += HEIGHT
 		y_text += HEIGHT #generating a line between paragraphs
 	bg.show()
 	bg.save('test.png')
 
+
+
+
+# Main function starts here
 bookFile = open("sense.txt")
 
 lineBucket = ''
