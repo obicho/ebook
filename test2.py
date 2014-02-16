0475b17e3e6de41621a27a5eb75956a76c28b6e8
obicho <cho@Tsais-MacBook-Pro.local>
2014-02-16 14:02:21 -0800

    removed debug prints and old code

--- test2.py
+++ test2.py
@@ -51,7 +51,6 @@ def main(argv):
 def cleanse(line):
     newline = line
     if (line.startswith(codecs.BOM_UTF8)):
-        print 'need cleansing'
         newline = line[3:]
     
     return newline;
@@ -74,29 +73,6 @@ def drawPage(text):
     #bg.save('test.png')
     epd.display(bg)
     epd.update()
-
-def demo(epd):
-    """simple drawing demo - black drawing on white background"""
-    # initially set all white background
-    image = Image.new('1', epd.size, WHITE)
-    # prepare for drawing
-    draw = ImageDraw.Draw(image)
-    w=200
-
-    font = ImageFont.load_default() 
-
-    text = "I've been reading through the Django Book, and in chapter 11 they talk about generating non-HTML content (such as PDF files, Images, RSS/Atom Feeds). They mention using PIL to generate images, but they don't give an example. So, I thought I'd post a simple example View that generates an image."
-    lines = textwrap.wrap(text,width = 200)
-    y_text =5 
-    for line in lines:
-  	width, height = font.getsize(line)
-	draw.text(((w - width)/2, y_text), line,font=font, fill = 'black')
-	y_text += height
-
-    # display image on the panel
-    epd.display(image)
-    epd.update()
-
 
 # main
 if "__main__" == __name__:
