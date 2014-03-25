240da31ed515b621b67922d2c775e1406129b110
Francis Wang <francis@bposolutions.com>
2014-03-24 17:43:19 -0700

    Add more tests

--- tests/test_text_formatter_interface.py
+++ tests/test_text_formatter_interface.py
@@ -14,5 +14,36 @@ class TestTextFormatterInterface(unittest.TestCase):
         # print self.t.getPage(0)
         self.assertGreater(len(self.t.getPage(0)), 100)
 
+    def testGetPageNavigate(self):
+        p1 = self.t.getPage(50)
+        p2 = self.t.getPage(50)
+        self.assertEquals(p1, p2)
+
+    def testGetPageNavigate2(self):
+        p1 = self.t.getPage(50)
+        p2 = self.t.getPage(51)
+        p3 = self.t.getPage(50)
+        p4 = self.t.getPage(51)
+        self.assertEquals(p1, p3)
+        self.assertEquals(p2, p4)
+
+    def testGetPageSimpleIntegrityCheck(self):
+        self.t.getPage(0)
+        pages = ''
+        for i in range(10):
+            pages += self.t.getPage(i)
+
+        fh = open('sense.txt')
+        original = fh.read(len(pages)*2)
+
+        original = self.t.cleanse(original.replace('\r\n', '').replace('\n', '').replace(' ', ''))
+        pages = pages.replace('\n', '').replace(' ', '')
+        # print ''
+        # print 'pages: \n' + pages
+        # print '-'*20
+        # print 'original: \n' + original
+
+        self.assertTrue(original.startswith(pages))
+
 if __name__ == '__main__':
     unittest.main()
